5f710e014d3f085aed1b7438b06a6efa1e7d4317
Omar AbuRish <191020@ppu.edu.ps>
2022-03-31 19:19:27 +0300

    added front end

--- front-end/front_end/lib/main.dart
+++ front-end/front_end/lib/main.dart
@@ -10,31 +10,67 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Maps Sample App'),
+              backgroundColor: Colors.green[700],
+            ),
+            body: Text("Hi")));
+  }
+}
+
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(9.669111, 80.014007);
+
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+
+    final marker = Marker(
+      markerId: MarkerId('place_name'),
+      position: LatLng(9.669111, 80.014007),
+      // icon: BitmapDescriptor.,
+      infoWindow: InfoWindow(
+        title: 'title',
+        snippet: 'address',
+      ),
+    );
+
+    setState(() {
+      markers[MarkerId('place_name')] = marker;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+          appBar: AppBar(
+            title: const Text('Maps Sample App'),
+            backgroundColor: Colors.green[700],
           ),
-        ),
-      ),
+          body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+            markers: markers.values.toSet(),
+          )),
     );
   }
 }
